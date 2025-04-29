e0dae8c7cb159f4a860841f7d2ce424cbce199c4
Erasyl <daurenoverasyl252@gmail.com>
2025-04-30 02:10:15 +0500

    Описание изменений

--- auto_show/settings.py
+++ auto_show/settings.py
@@ -88,17 +88,13 @@ WSGI_APPLICATION = 'auto_show.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'autoshowdatabase',
-        'USER': 'daurenov',
-        'PASSWORD': '97Erasyl00',
-        'HOST': 'daurenov.postgres.pythonanywhereservices.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
